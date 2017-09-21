8b76f29b6fc6f37c4db2c3a267fe6d7e3e898495
Alex Webb <alexwebbb@protonmail.com>
2017-09-21 03:22:51 -0500

    more robust version

--- layer step down.jsx
+++ layer step down.jsx
@@ -4,22 +4,26 @@ var doc = activeDocument;
 var layNum;
 
 var aLay = new Array(doc.layers.length);
+var layerParent = doc.activeLayer.parent;
 
+var isParent = false;
+
+if ( layerParent !== doc ) {
+	doc.activeLayer = layerParent;
+	isParent = true;
+}
+	
 for(var i=0; i < doc.layers.length; i++) {
 	// get the name for that layer and 
 	// assign to equivalent position in the array
     aLay[i] = doc.layers[i].name;
     // if layer is active one it gets stored as the current layer
-    if(aLay[i] == doc.activeLayer.parent.name) { layNum = i }
+    if(aLay[i] == doc.activeLayer.name) { layNum = i }
 };
 
 if(doc.layers[layNum+1].name.indexOf("Layer") >= 0) {
 
-
-	var layerParent = doc.activeLayer.parent;
-	if ( layerParent !== doc ) doc.activeLayer = layerParent;
-
-    doc.activeLayer.parent.visible = false;
-    doc.activeLayer = doc.layers[layNum+1].layers[0];
+    doc.activeLayer.visible = false;
+    doc.activeLayer = isParent ? doc.layers[layNum+1].layers[0] : doc.layers[layNum+1];
     doc.activeLayer.parent.visible = true;
 };
--- layer step up.jsx
+++ layer step up.jsx
@@ -1,18 +1,29 @@
 ﻿#target photoshop
 
 var doc = activeDocument;
-var cLay = doc.activeLayer;
 var layNum;
 
 var aLay = new Array(doc.layers.length);
+var layerParent = doc.activeLayer.parent;
 
+var isParent = false;
+
+if ( layerParent !== doc ) {
+	doc.activeLayer = layerParent;
+	isParent = true;
+}
+	
 for(var i=0; i < doc.layers.length; i++) {
+	// get the name for that layer and 
+	// assign to equivalent position in the array
     aLay[i] = doc.layers[i].name;
-    if(aLay[i] == cLay.name) { layNum = i }
-    };
+    // if layer is active one it gets stored as the current layer
+    if(aLay[i] == doc.activeLayer.name) { layNum = i }
+};
 
 if(doc.layers[layNum-1].name.indexOf("Layer") >= 0) {
-        doc.activeLayer.visible = false;
-        doc.activeLayer = doc.layers[layNum-1];
-        doc.activeLayer.visible = true;
-        }
+
+    doc.activeLayer.visible = false;
+    doc.activeLayer = isParent ? doc.layers[layNum-1].layers[0] : doc.layers[layNum-1];
+    doc.activeLayer.parent.visible = true;
+};
