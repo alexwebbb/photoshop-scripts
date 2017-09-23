d59aa86c9b323c03183900b16dea0dac18447d69
Alex Webb <alexwebbb@protonmail.com>
2017-09-23 09:25:28 -0500

    made some aesthetic improvements

--- layer step down.jsx
+++ layer step down.jsx
@@ -9,7 +9,7 @@ if ( layerParent !== doc ) itIsAParent = true;
     
 // here we just identify what index the active layer or layer group is in its parent
 for (var i = doc.layers.length - 1; i >= 0; i--) {
-    if(doc.layers[i].name.match((itIsAParent ? doc.activeLayer.parent.name : doc.activeLayer.name))) {
+    if(doc.layers[i] === (itIsAParent ? doc.activeLayer.parent : doc.activeLayer)) {
         layNum = i;
         break;
     }
@@ -22,18 +22,23 @@ if(!doc.layers[layNum+1].name.match("root")) {
     doc.activeLayer.pixelsLocked = true;
 
     // want two different behaviors based on whether document is a series of folders or just artboards
-    (itIsAParent ? doc.activeLayer.parent : doc.activeLayer).visible = false;
-
     if(itIsAParent) {
+
+        doc.activeLayer.parent.visible = false;
+
         // this if switch is just here to recenter the view when there are four or more elements
         if(doc.layers[layNum+1].layers.length > 3) {
-            doc.activeLayer = doc.layers[layNum+1].layers[3];
-            doc.activeLayer = doc.layers[layNum+1].layers[0];    
+            doc.activeLayer = doc.layers[layNum+1].layers[3];  
         }
+
         // here we return to the active layer, which in my scheme is the second layer from the top
     	doc.activeLayer = doc.layers[layNum+1].layers[1];
     	doc.activeLayer.parent.visible = true;
+        
     } else {
+
+        doc.activeLayer.visible = false;
+
         // this case is for the scenario where it is just artboards... a standard notepad
     	doc.activeLayer = doc.layers[layNum+1];
     	doc.activeLayer.visible = true;
